d7f616d01828b184fa1cb79de5b175cda0a1d1f1
codewithpassion <dominik.fretz@gmail.com>
2014-09-25 22:56:43 -0700

    cleanup

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-CONFIG=${PWD#}/openrov-com
-echo Loading docker configuration from: ${CONFIG#}/
-docker run -d -p 2000:2368 -v ${CONFIG}:/ghost-override --name='openrov-ghost' dockerfile/ghost
